c42bad6c0d26b3fe17c84cb0fb8c2eb112660c6d
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-12-22 04:46:13 +0100

    Changed createUserMap HttpResponse status to created 201

--- map-service/src/main/java/com/infomap/mapservice/map/MapController.java
+++ map-service/src/main/java/com/infomap/mapservice/map/MapController.java
@@ -2,6 +2,8 @@ package com.infomap.mapservice.map;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,7 +22,7 @@ public class MapController {
     }
 
     @PostMapping
-    public Map createUserMap(@Valid @RequestBody Map map) {
-        return mapService.createUserMap(map);
+    public ResponseEntity<Map> createUserMap(@Valid @RequestBody Map map) {
+        return new ResponseEntity<>(mapService.createUserMap(map), HttpStatus.CREATED);
     }
 }
