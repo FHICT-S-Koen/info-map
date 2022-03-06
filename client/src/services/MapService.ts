a1f66c35590d58f86812d3689384d3680271c9f4
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-03-06 20:43:31 +0100

    Switched over to vite

--- client/src/services/MapService.ts
+++ client/src/services/MapService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Map from '../models/Map'
 
 export default class MapService {
-  static API_URL = process.env.REACT_APP_PROXY_URL
+  static API_URL = import.meta.env.VITE_PROXY_URL
 
   public static async getUserMap(userId: string): Promise<Map> {
     const response = await axios.get<Map>(`${this.API_URL}/map/${userId}`)
--- client/src/services/NoteService.ts
+++ client/src/services/NoteService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Note from '../models/Note'
 
 export default class NoteService {
-  static API_URL = process.env.REACT_APP_PROXY_URL
+  static API_URL = import.meta.env.VITE_PROXY_URL
 
   public static async getMapNotes(mapId: string): Promise<Note[]> {
     const response = await axios.get<Note[]>(`${this.API_URL}/note/${mapId}`)
