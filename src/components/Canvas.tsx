49ceeadb5bd31d38005ca9f8829b5eb55b7244ed
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-04-09 23:47:41 +0200

    Added scrolling, needs refactoring

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,28 +3,40 @@ import { clearCanvas } from "../utils"
 
 const Canvas = () => {
 	const [{x, y}, _setCoords] = useState({x: 0, y: 0})
+	const [zoom, _setZoom] = useState(1.00)
 	const canvasRef = useRef<HTMLCanvasElement>(null)
 	const coordsRef = useRef({x, y})
+	const zoomRef = useRef(zoom)
 
 	const setCoords = (x: number, y: number) => {
 		coordsRef.current = {x, y}
-		_setCoords({x: x, y: y})
+		_setCoords({x, y})
+	}
+
+	const setZoom = (z: number) => {
+		zoomRef.current = z
+		_setZoom(z)
 	}
 
 	const drawSquares = (context: CanvasRenderingContext2D) => {
-		const {x, y} = coordsRef.current
-		context.beginPath()
-		context.moveTo(50 + x, 50 + y)
-		context.lineTo(50 + x, 100 + y)
-		context.lineTo(100 + x, 100 + y)
-		context.lineTo(100 + x, 50 + y)
-		context.lineTo(50 + x, 50 + y)
-		context.moveTo(120 + x, 50 + y)
-		context.lineTo(120 + x, 100 + y)
-		context.lineTo(170 + x, 100 + y)
-		context.lineTo(170 + x, 50 + y)
-		context.lineTo(120 + x, 50 + y)
-		context.stroke()
+		if (canvasRef.current) {
+			const canvas = canvasRef.current
+			let {x, y} = coordsRef.current
+			const {width, height} = canvas.getBoundingClientRect()
+			// console.log(width, height)
+			x -= Math.round(width * 0.5)
+			y += Math.round(height * 0.5)
+			// console.log(x, y)
+			const z = zoomRef.current
+			context.beginPath()
+			context.moveTo(-25*z - x, -25*z + y)
+			context.lineTo(-25*z - x, 25*z + y)
+			context.lineTo(25*z - x, 25*z + y)
+			context.lineTo(25*z - x, -25*z + y)
+			context.lineTo(-25*z - x, -25*z + y)
+
+			context.stroke()
+		}
 	}
 
 	const resetCoords = () => {
@@ -75,7 +87,31 @@ const Canvas = () => {
 				if (e.shiftKey) return
 
 				// if (0 < e.clientX && e.clientX < 100 && 0 < e.clientY && e.clientY < 100) return
-				setCoords(x+e.movementX, y+e.movementY)
+				setCoords(Math.round(x-e.movementX/zoom), Math.round(y+e.movementY/zoom))
+				clearCanvas(canvas)
+				drawSquares(context)
+			}
+		}
+	}
+
+	const handleScroll = (e: React.WheelEvent<HTMLCanvasElement>) => {
+		if (canvasRef.current) {
+			const canvas = canvasRef.current
+			const context = canvas.getContext('2d')
+			if (context) {
+				const {width, height} = canvas.getBoundingClientRect()
+				const ox = e.screenX/width //TODO: doesn't work when using smaller window
+				const oy = (e.screenY-188)/height  //TODO: make this dynamic
+				if (e.deltaY > 0 && zoom > 0.11) {
+					setZoom(zoom - 0.05)
+					setCoords(ox < 0.5 ? x + Math.round(50 + (ox * -100)) : x - Math.round(50 - (100 - (ox * 100))) , oy < 0.5 ? y - Math.round(50 + (oy * -100)) : y + Math.round(50 - (100 - (oy * 100)))) //TODO: refactor
+				}  
+				else if (e.deltaY < 0 && zoom < 4.9) {
+					setZoom(zoom + 0.05)
+					setCoords(ox < 0.5 ? x - Math.round(50 + (ox * -100)) : x + Math.round(50 - (100 - (ox * 100))) , oy < 0.5 ? y + Math.round(50 + (oy * -100)) : y - Math.round(50 - (100 - (oy * 100))))
+				}
+				
+				resizeCanvasToWindowSize(canvas)
 				clearCanvas(canvas)
 				drawSquares(context)
 			}
@@ -86,12 +122,13 @@ const Canvas = () => {
 			id="canvas"
 			className="flex-grow"
 			ref={canvasRef}
+			onWheel={handleScroll}
 			onMouseMove={handleMouseMove}>
 		</canvas>
 		<span /*TODO: fix location, styling and light theme*/
 			onClick={resetCoords} 
 			className="absolute top-4 right-4 font-bold x-50 text-white">
-			{`x: ${x}, y: ${y}`}
+			{`x: ${x}, y: ${y}, z: ${Math.round(zoom*100)}%`}
 		</span>
 	</>
 }
