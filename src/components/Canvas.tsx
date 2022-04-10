99ae1a7205e5895d2103477f6b9c076893440149
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-04-10 19:47:11 +0200

    Added text example and improved reset buttons

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,7 +9,6 @@ const Canvas = () => {
 	const setCoords = (x: number, y: number) => {
 		coordsRef.current = {x, y}
 		_setCoords({x, y})
-		// console.log("xy:",x, y)
 	}
 
 	const [zoom, _setZoom] = useState(100)
@@ -17,10 +16,9 @@ const Canvas = () => {
 	const setZoom = (z: number) => {
 		zoomRef.current = z
 		_setZoom(z)
-		// console.log("z:",z)
 	}
 
-	const [note, _setNote] = useState({x: -25, y: -25, w: 50, h: 50, t: "test"})
+	const [note, _setNote] = useState({x: -25, y: -25, w: 50, h: 50, t: "test", f: 10})
 
 	const resetCoords = () => {
 		if (canvasRef.current) {
@@ -28,6 +26,17 @@ const Canvas = () => {
 			const context = canvas.getContext('2d')
 			if (context) {
 				setCoords(0, 0)
+				draw(context)
+			}
+		}
+	}
+
+	const resetZoom = () => {
+		if (canvasRef.current) {
+			const canvas = canvasRef.current
+			const context = canvas.getContext('2d')
+			if (context) {
+				setZoom(100)
 				draw(context)
 			}
 		}
@@ -55,15 +64,13 @@ const Canvas = () => {
 
 			clearCanvas(canvas)
 
-			console.log((-(note.w * z/100) + note.w) * 0.5)
-
 			const offsetX = (-(note.w * z/100) + note.w) * 0.5
 			const offsetY = (-(note.h * z/100) + note.h) * 0.5
 			
-
 			context.beginPath()
 			context.rect(note.x + x + offsetX , note.y + y + offsetY, note.w * z/100, note.h * z/100)
-			context.fillText("Test", note.x + x + offsetX, note.y + 10 + y + offsetY);
+			context.font = note.f * z/100 + 'px ubuntu'
+			context.fillText("Test", note.x + x + offsetX, note.y + note.f * z/100 + y + offsetY);
 			context.rect(-5 + Math.round(width * 0.5), -5 + Math.round(height * 0.5), 10, 10)
 			context.stroke()
 		}
@@ -100,8 +107,8 @@ const Canvas = () => {
 				if (e.shiftKey) return
 
 				// if (0 < e.clientX && e.clientX < 100 && 0 < e.clientY && e.clientY < 100) return
-				// console.log(Math.round(x-e.movementX/zoom), Math.round(y+e.movementY/zoom))
-				setCoords(x + e.movementX, y + e.movementY)
+
+				setCoords(x + e.movementX, y + e.movementY) //TODO: zoom does not effect coords
 				draw(context)
 			}
 		}
@@ -119,12 +126,12 @@ const Canvas = () => {
 				if (e.deltaY > 0 && zoom > 1) {
 					setZoom(Math.round(zoom - 1))
 
-					setCoords(ox < 0.5 ? x - Math.round(50 + (ox * -100)) : x + Math.round(50 - (100 - (ox * 100))) , oy < 0.5 ? y - Math.round(50 + (oy * -100)) : y + Math.round(50 - (100 - (oy * 100)))) //TODO: refactor
+					setCoords(ox < 0.5 ? x - Math.round(zoom/2 + (ox * -zoom)) : x + Math.round(zoom/2 - (zoom - (ox * zoom))) , oy < 0.5 ? y - Math.round(zoom/2 + (oy * -zoom)) : y + Math.round(zoom/2 - (zoom - (oy * zoom)))) //TODO: refactor
 				}
 				else if (e.deltaY < 0) {
 					setZoom(Math.round(zoom + 1))
 					
-					setCoords(ox < 0.5 ? x + Math.round(50 + (ox * -100)) : x - Math.round(50 - (100 - (ox * 100))) , oy < 0.5 ? y + Math.round(50 + (oy * -100)) : y - Math.round(50 - (100 - (oy * 100))))
+					setCoords(ox < 0.5 ? x + Math.round(zoom/2 + (ox * -zoom)) : x - Math.round(zoom/2 - (zoom - (ox * zoom))) , oy < 0.5 ? y + Math.round(zoom/2 + (oy * -zoom)) : y - Math.round(zoom/2 - (zoom - (oy * zoom))))
 				}
 
 				draw(context)
@@ -137,13 +144,19 @@ const Canvas = () => {
 			className="flex-grow"
 			ref={canvasRef}
 			onWheel={handleScroll}
-			onMouseMove={handleMouseMove}>
+			onMouseMove={handleMouseMove}
+			>
 		</canvas>
-		<span /*TODO: fix location, styling and light theme*/
-			onClick={resetCoords} 
-			className="absolute top-4 right-4 font-bold x-50 text-white">
-			{`x: ${x}, y: ${y}, z: ${zoom}%`}
-		</span>
+		<button 
+			onClick={resetCoords}
+			className="absolute top-4 right-20 dark:text-slate-500 dark:bg-slate-800 font-bold x-50 shadow p-[6px] rounded-md text-black"
+			>x: {x}, y: {y}
+		</button>
+		<button
+			onClick={resetZoom}
+			className="absolute top-4 right-4 dark:text-slate-500 dark:bg-slate-800 font-bold x-50 shadow p-[6px] rounded-md text-black"
+		>{zoom}%
+		</button>
 	</>
 }
 
